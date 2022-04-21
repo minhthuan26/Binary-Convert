a5b7ef46a5080cccb29ef85ff9ba292c14f1da18
Lê Minh Thuận <minhthuan26042001@gmail.com>
2022-04-22 02:32:57 +0700

    hoàn thành

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,18 +28,10 @@ namespace ChuyenDoiCoSo_WPF
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-        private bool _dec = false;
-        public bool Dec { get { return _dec; } set { _dec = value; OnPropertyChanged(); } }
-
-        private bool _bin = false;
-        public bool Bin { get { return _bin; } set { _bin = value; OnPropertyChanged(); } }
-
-        private bool _hex = false;
-        public bool Hex { get { return _hex; } set { _hex = value; OnPropertyChanged(); } }
-
         public MainWindow()
         {
             InitializeComponent();
+            switched();
         }
         private void IsEnable(object sender, RoutedEventArgs e)
         {
@@ -48,27 +40,159 @@ namespace ChuyenDoiCoSo_WPF
 
         private void switched()
         {
-            switch (from.Text)
+            if (dec != null && hex != null && bin != null)
+                switch (from.SelectedIndex)
+                {
+                    case 0:
+                        dec.IsEnabled = true;
+                        hex.IsEnabled = false;
+                        bin.IsEnabled = false;
+                        break;
+
+                    case 1:
+                        dec.IsEnabled = false;
+                        hex.IsEnabled = true;
+                        bin.IsEnabled = false;
+                        break;
+
+                    case 2:
+                        dec.IsEnabled = false;
+                        hex.IsEnabled = false;
+                        bin.IsEnabled = true;
+                        break;
+                }
+        }
+
+        private void Exchange(object sender, RoutedEventArgs e)
+        {
+            int f = from.SelectedIndex;
+            int t = to.SelectedIndex;
+            if (f == t)
             {
-                case "Thập phân":
-                    dec.IsEnabled = true;
-                    hex.IsEnabled = false;
-                    bin.IsEnabled = false;
-                    break;
-
-                case "Thập lục phân":
-                    dec.IsEnabled = false;
-                    hex.IsEnabled = true;
-                    bin.IsEnabled = false;
-                    break;
-
-                case "Nhị phân":
-                    dec.IsEnabled = false;
-                    hex.IsEnabled = false;
-                    bin.IsEnabled = true;
-                    break;
+                MessageBox.Show("Hai hệ cơ số phải khác nhau.");
+                bin.Text = hex.Text = dec.Text = "";
+            }
+            else if (f == 0 && t == 1)
+            {
+                int tmp;
+                while (true)
+                {
+                    try
+                    {
+                        tmp = Int32.Parse(dec.Text);
+                        hex.Text = tmp.ToString("X");
+                        break;
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Số hệ 10 phải là số thực.");
+                        break;
+                    }
+                    
+                }
+                
+            }
+            else if (f == 0 && t == 2)
+            {
+                int tmp;
+                while (true)
+                {
+                    try
+                    {
+                        tmp = Int32.Parse(dec.Text);
+                        bin.Text = Convert.ToString(tmp, 2);
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Sai dữ liệu.");
+                        break;
+                    }
+                }
+            }
+            else if (f == 1 && t == 0)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        dec.Text = int.Parse(hex.Text, System.Globalization.NumberStyles.HexNumber).ToString();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Sai dữ liệu.");
+                        break;
+                    }
+                }
+            }
+            else if (f == 1 && t == 2)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        bin.Text = Convert.ToString(Convert.ToInt32(hex.Text, 16), 2);
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Sai dữ liệu.");
+                        break;
+                    }
+                }
+            }
+            else if (f == 2 && t == 0)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        dec.Text = Convert.ToInt32(bin.Text, 2).ToString();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Sai dữ liệu.");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    int tmp;
+                    try
+                    {
+                        tmp = Convert.ToUInt16(bin.Text, 2);
+                        hex.Text = tmp.ToString("X");
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Sai dữ liệu.");
+                        break;
+                    }
+                }
             }
         }
 
+        private void ClearAll(object sender, RoutedEventArgs e)
+        {
+            bin.Text = hex.Text = dec.Text = "";
+        }
+
+        private void Swap(object sender, RoutedEventArgs e)
+        {
+            if(dec.Text != null && hex.Text != null && bin.Text != null)
+            {
+                bin.Text = hex.Text = dec.Text = "";
+            }
+            
+            int tmp1 = from.SelectedIndex;
+            from.SelectedIndex = to.SelectedIndex;
+            to.SelectedIndex = tmp1;
+        }
     }
 }
